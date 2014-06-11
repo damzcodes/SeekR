90329857f926aa66ae3f2ef6791c96f4a6717a39
Damilola Odelola <damiiodelola@gmail.com>
2014-06-11 17:20:14 +0100

    user can log in

--- app/controllers/User.rb
+++ app/controllers/User.rb
@@ -1,23 +1,17 @@
 SeekR::App.controllers :user do
   
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
+  
+  get :index do
+    render 'index'
+  end
 
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+  get :login do
+    render 'login'
+  end
 
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
+  post :login do
+    redirect_to '/user'
+  end 
   
 
 end
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,21 +1,22 @@
 Given(/^a User "(.*?)" is already registered$/) do |user|
-  user = User.create!(:name => "NGO_User", :email => "ngo_user@example.org", :password => "password")
+  user = User.create!(:organisation => "Red Cross", :email => "ngo_user@example.org", :password => "password")
 end
 
 Given(/^I am on the homepage$/) do
-  visit '/'
+  visit '/user'
 end
 
 Given(/^I follow the login link$/) do
-  pending # express the regexp above with the code you wish you had
+  click_on 'login'
 end
 
-When(/^I enter my email as "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I enter my email as "(.*?)"$/) do |email|
+	save_and_open_page
+  fill_in 'Email', :with => email
 end
 
-When(/^my password is "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^my password is "(.*?)"$/) do |password|
+  fill_in 'Password', :with => password
 end
 
 Then(/^I should be logged in$/) do
